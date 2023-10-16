a55fdf164c2ea72cb07753082582b9437d6a8e5b
MamoonRashidRazmal <mamoonrashidrazmal@mamoonrzmalsair.localdomain>
2023-10-16 15:29:51 +0200

    sd

---
+++ controllers/postsControllers.js
@@ -0,0 +1,34 @@
+const Post=require("../models/Posts")
+const getAllPosts=async(req,res)=>{
+    try{
+        const user_id=req.user._id
+       // const user_id=req.user._id;
+        const Posts=await Post.find({user_id})
+        res.status(200).json(Posts)
+    }
+    catch(error){
+        res.status(500).json({error:error.message})
+
+    }
+}
+const createPost=async(req,res)=>{
+    const{title,body}=req.body
+    let emptyFields=[]
+    if(!title){
+        emptyFields.push("title")
+    }
+    if(!body){
+        emptyFields.push("body")
+    }
+    if(emptyFields.length>0){
+        return res.status(400).json({error:"please fill all fields"})
+    }
+    try{
+        const user_id=req.user._id
+        const post=await Post.create({title,body,user_id})
+        res.status(201).json(post)
+    }catch(error){
+        res.status(500).json({error:error.message})
+    }
+}
+module.exports={getAllPosts,createPost}
